f6bd487ed4ffc9d53da1925f60a88d6704e93e4f
Nedaa Gehad <nedaagehad@gmail.com>
2022-07-02 22:21:20 +0200

    Replacing NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-// import Button from 'react-bootstrap/Button';
 import { NavLink, Link } from "react-router-dom";
 // import Form from 'react-bootstrap/Form';
 // import FormControl from 'react-bootstrap/FormControl';
