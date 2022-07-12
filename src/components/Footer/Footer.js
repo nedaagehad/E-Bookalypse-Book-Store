8e2fd6bf121a966aa24730e224da753f776c5cc2
Nedaa Gehad <nedaagehad@gmail.com>
2022-07-12 22:04:16 +0200

    changing scrollbar & finishing footer-dark layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,36 +5,39 @@ import { RiInstagramFill } from 'react-icons/ri'
 
 function Footer() {
   return (
-    <div className="container-fluid bg-dark text-light px-5 py-3">
+    <div className="container-fluid bg-dark text-light px-5 py-3 position-relative">
+      <p className="copyrights">© 2022 Copyright E-Bookalypse | All Rights Reserved</p>
       <div className='row justify-content-center'>
-        <div className='col-3'>
+        <div className='col-3 footer-item text-center'>
           <h5>Support</h5>
           <ul className='list-unstyled'>
             <li>About Us</li>
-            <li>Refund Policy</li>
-            <li>Terms of Use</li>
-            <li>Payment Policy</li>
-            <li>Privacy Policy</li>
             <li>Contact Us</li>
+            <li>Publish with us</li>
           </ul>
         </div>
-        <div className='col-5 d-flex align-items-center justify-content-center border border-danger'>
-          <p>© 2022 Copyright E-Bookalypse | All Rights Reserved</p>
+        <div className='col-5 d-flex align-items-center justify-content-center'>
+          <div className='footer-item'>
+            <span>Terms of Use</span> | &nbsp;
+            <span>Refund Policy</span> | &nbsp;
+            <span>Payment Policy</span> | &nbsp;
+            <span>Privacy Policy</span>
+          </div>
         </div>
-        <div className='col-4 text-center'>
+        <div className='col-4 text-center footer-item'>
           <h5>Connect with us</h5>
-          <div className='row p-1'>
-            <div className='col-3'>
-              <FaFacebookF className='media-icon' />
+          <div className='row justify-content-center'>
+            <div className='col-3 media-icon-container'>
+              <a href='http://www.facebook.com' target="_blank" ><FaFacebookF className='media-icon-night' /></a>
             </div>
-            <div className='col-3'>
-              <BsTwitter className='media-icon' />
+            <div className='col-3 media-icon-container'>
+              <a href='http://www.twitter.com' target="_blank" ><BsTwitter className='media-icon-night' /></a>
             </div>
-            <div className='col-3'>
-              <RiInstagramFill className='media-icon' />
+            <div className='col-3 media-icon-container'>
+              <a href='http://www.instagram.com' target="_blank"><RiInstagramFill className='media-icon-night' /></a>
             </div>
-            <div className='col-3'>
-              <FaLinkedinIn className='media-icon' />
+            <div className='col-3 media-icon-container'>
+              <a href='http://www.linkedin.com' target="_blank"><FaLinkedinIn className='media-icon-night' /></a>
             </div>
           </div>
         </div>
