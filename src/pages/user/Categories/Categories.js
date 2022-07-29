374b790bbfcc5358094c1fbf476cf681093407f8
Reham <rehamraafat2525@gmail.com>
2022-07-30 01:50:28 +0200

    category entry page - category list

--- src/pages/user/Categories/Categories.js
+++ src/pages/user/Categories/Categories.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import TrendingBooksUp1 from '../../../components/TrendingBooksUp/TrendingBooksUp1';
+import CategoryList from '../../../components/CategoryList/CategoryList'
+import CategoryCard from '../../../components/CategoryCard/CategoryCard'
 function Categories() {
   return (
     <div className='content'>
-      
+          <CategoryList>
+              <CategoryCard img="./Images/Categories/Biography.jpg" alt="Biography"/>
+              <CategoryCard img="./Images/Categories/Children.jpg" alt="Children" />
+              <CategoryCard img="./Images/Categories/horror.jpg" alt="Horror" />
+              <CategoryCard img="./Images/Categories/history.jpg" alt="History" />
+              <CategoryCard img="./Images/Categories/poeatries.jpg" alt="Poetry" />
+              <CategoryCard img="./Images/Categories/Novels.jpg" alt="Novels" />
+          </CategoryList>
+          <TrendingBooksUp1/>
     </div>
   )
 }
