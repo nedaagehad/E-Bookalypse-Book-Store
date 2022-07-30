4e5ef329a8170b930c793b994f90591cd2021b8d
Reham <rehamraafat2525@gmail.com>
2022-07-31 00:18:09 +0200

    filterbar added in category

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,10 +1,103 @@
 import React from 'react'
 import classes from './FilterBar.module.css'
-
-const FilterBar = props =>{
-    return(
-        <div className="col-md-3 col-sm-12">
-            
+import PriceRange from './PriceRange/PriceRange'
+const FilterBar = props => {
+    return (
+        <div className={`col-md-3 col-sm-12 ${classes.FilterBar}`}>
+            <h2>Filter</h2>
+            <div className={classes.Filters}>
+                <h3>Categories</h3>
+                <div className={classes.cat_Filter}>    
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            All Categories
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Biography
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Children
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Horror
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            History & Culture
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Scientific Researches
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Novels
+                        </label>
+                    </div>
+                </div>
+                <h3>Rates</h3>
+                <div className={classes.cat_Rate}>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            4 stars or above
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            3 stars or above
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            2 stars or above
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            1 stars or above
+                        </label>
+                    </div>
+                </div>
+                <h3>Sort Price</h3>
+                <div className={classes.cat_Sort_Price}>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Low to High
+                        </label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+                        <label class="form-check-label" for="flexCheckDefault">
+                            High to Low
+                        </label>
+                    </div>
+                </div>
+                <h3>Price Range</h3>
+                <div className={classes.cat_Price_Range}>
+                    <PriceRange/>
+                </div>
+            </div>
         </div>
     )
 }
