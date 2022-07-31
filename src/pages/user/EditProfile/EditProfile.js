51a24c8bc2ed0fe7b3c69f4621aae0ecebc556f5
Nedaa Gehad <nedaagehad@gmail.com>
2022-07-31 10:59:35 +0200

    ranan's work - editProfile

---
+++ src/pages/user/EditProfile/EditProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditUserProfile from '../../../components/EditUserProfile/EditUserProfile';
+
+function EditProfile() {
+  return (
+    <div>
+      <EditUserProfile />
+    </div>
+  )
+}
+
+export default EditProfile
