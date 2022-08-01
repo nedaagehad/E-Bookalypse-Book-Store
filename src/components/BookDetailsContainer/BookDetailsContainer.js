2198b036362b2a217ce42a57c289eda73c254782
Reham <rehamraafat2525@gmail.com>
2022-08-01 17:49:26 +0200

    95% cook details

--- src/components/BookDetailsContainer/BookDetailsContainer.js
+++ src/components/BookDetailsContainer/BookDetailsContainer.js
@@ -1,10 +1,51 @@
 import React from 'react'
 import classes from './BookDetailsContainer.module.css'
+import { BsFillHeartFill } from 'react-icons/bs'
+import { AiFillStar } from 'react-icons/ai'
 
 const BookDetailsContainer = props => {
     return (
         <div className={`col-12`}>
-
+            <div className={classes.BookDetails}>
+                <h2>Book Details</h2>
+                <div className={`row`}>
+                    <div className={`col-md-3 col-sm-12`}>
+                        <div className={classes.Book_Poster}>
+                            <img className="img-thumbnail" src="../../Images/Books/1.jpg" alt=""/>
+                        </div>
+                    </div>
+                    <div className={`col-md-9 col-sm-12`}>
+                        <div className={classes.Details}>
+                            <div className={classes.Reviews}>
+                                <button className={classes.stars}>
+                                    <span><AiFillStar /></span>
+                                    <span><AiFillStar /></span>
+                                    <span><AiFillStar /></span>
+                                    <span><AiFillStar /></span>
+                                    <span><AiFillStar /></span>
+                                    &nbsp;&nbsp;4.2
+                                </button>
+                                <button className={classes.Review}>56 Reviews</button>
+                            </div>
+                            <h1>Harry Potter and the Sorcerer’s Stone </h1>
+                            <h3>J. K. Rowling</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+                                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                 pariatur. Excepteur sint occaecat cupidatat non proident,
+                                sunt in culpa qui officia deserunt mollit anim id est laborum."
+                            </p>
+                            <h2>$15,50<span className={classes.promo}><sub><del>$ 19.98</del></sub></span></h2>
+                            <div className={classes.action}>
+                                <button>Add To Card <i className={" col-2 align-self-start bi bi-basket2-fill  text-white text-center rounded-circle py-1 mt-1 "}></i></button>
+                                <button className={classes.favorite}><BsFillHeartFill/></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/BooksView/BooksView.js
+++ src/components/BooksView/BooksView.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './BooksView.module.css'
 import BookCard from '../BookCard/BookCard'
+import Pagination from '@mui/material/Pagination';
 
 const BookView = props =>{
     return(
@@ -13,6 +14,11 @@ const BookView = props =>{
                 <BookCard img="../../Images/Books/4.jpg" alt="Harry Potter and the philospher stone" price="$15.50"/>
                 <BookCard img="../../Images/Books/1.jpg" alt="Harry Potter and the philospher stone" price="$15.50"/>
                 <BookCard img="../../Images/Books/2.jpg" alt="Harry Potter and the philospher stone" price="$15.50"/>
+                <div className={`col-12`}>
+                    <div className={classes.pagy}>
+                        <Pagination count={5} color="secondary" />
+                    </div>
+                </div>
             </div>
         </div>
     )
