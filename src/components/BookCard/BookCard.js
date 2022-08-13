a66b0d81a44a9207e86936977b8a01869cc2a699
Nedaa Gehad <nedaagehad@gmail.com>
2022-08-13 17:56:08 +0200

    merging with reham 13/8/22

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -1,5 +1,5 @@
 
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import classes from './BookCard.module.css'
 // <<<<<<< HEAD
@@ -10,47 +10,49 @@ import { Link } from 'react-router-dom';
 import AddToCardButton from '../AddToCardButton/AddToCardButton'
 // >>>>>>> 064fd04123a5f582be55b3c12a9a48eb0b37d657
 
-const  BookCard = props => {
+const BookCard = props => {
   // console.log(props.book)
-  const [ images,setImages] = useState()
+  const [images, setImages] = useState()
   const theme = useSelector((state) => state.theme.currentTheme);
-  useEffect(() => {
-    const starsRef = ref(storage, `/uploads/books/poster/${props.book.poster}`);
-     getDownloadURL(starsRef).then( (url)=>{
-      const newUrl = url
-   
-      setImages(newUrl)
-      
-    }).catch((error) => {console.log(error)});
-  }, []);
+  // useEffect(() => {
+  //   const starsRef = ref(storage, `/uploads/books/poster/${props.book.poster}`);
+  //   getDownloadURL(starsRef).then((url) => {
+  //     const newUrl = url
+
+  //     setImages(newUrl)
+
+  //   }).catch((error) => { console.log(error) });
+  // }, []);
 
 
 
   return (
-// <<<<<<< HEAD
-    
     <div className={theme === "night" ? classes.cardNight : classes.card}>
-          <Link to={'/books/BookDetails/'+props.book._id} className={`${classes.BookCard}`}>
+      {/* ******************************HUSSEIN'S DYNAMIC CODE******************************** */}
+      {/* <Link to={'/books/BookDetails/'+props.book._id} className={`${classes.BookCard}`}>
             <div className={classes.poster}>
                 <div className={classes.poster_overlay}></div>
                 <img className={classes.Book_Poster} src={images} alt={props.alt}/>
             </div>
             </Link>
             <h5 className={theme === "night" ? classes.titleNight : ""}>{props.alt}</h5>
-            <h3>{props.book.price}</h3>
-            {/* <button>Add to Card <i className={classes.basketIcon + " col-2 align-self-start bi bi-basket2-fill  text-white text-center rounded-circle py-1 mt-1 "}></i></button> */}
-{/* ======= */}
-    {/* <div className={classes.BookCard}>
-      <div className={classes.card}>
-            <div className={classes.poster}>
-                <div className={classes.poster_overlay}></div>
-                <img className={classes.Book_Poster} src={props.img} alt={props.alt}/>
-            </div>
-            <h5>{props.alt}</h5>
-            <h3>{props.price}</h3> */}
-            <AddToCardButton/>
-{/* >>>>>>> 064fd04123a5f582be55b3c12a9a48eb0b37d657 */}
+            <h3>{props.book.price}</h3> */}
+              {/* *************************************************************************** */}
+
+            {/* **********************REHAM'S STATIC CODE***************************** */}
+      <div className={classes.BookCard}>
+        <div className={classes.card}>
+          <div className={classes.poster}>
+            <div className={classes.poster_overlay}></div>
+            <img className={classes.Book_Poster} src={props.img} alt={props.alt} />
+          </div>
+          <h5>{props.alt}</h5>
+          <h3>{props.price}</h3>
+          {/* ***************************************************************************** */}
+          <AddToCardButton />
         </div>
+      </div>
+    </div>
   )
 }
 
