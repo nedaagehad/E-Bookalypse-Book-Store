6c177219ce9adea07c335bf1e54a2598e5b53291
Ranan <ranaanhosney3@gmail.com>
2022-08-21 03:19:22 +0200

    dashboard with module.css file

--- src/components/AdminNavbar/AdminNavbar.js
+++ src/components/AdminNavbar/AdminNavbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import '../../assetsAdmin/css/style.css'
+import dashstyle from '../../assetsAdmin/css/style.module.css'
 import "../../assetsAdmin/vendors/mdi/css/materialdesignicons.min.css"
 import "../../assetsAdmin/vendors/flag-icon-css/css/flag-icon.min.css"
 import "../../assetsAdmin/AdminDashboard.css";
@@ -15,47 +15,47 @@ function AdminNavbar(props) {
       <div>
         <div className="page-body-wrapper">
 
-          <nav className="navbar p-0 fixed-top d-flex flex-row">
-            <div className="navbar-brand-wrapper d-flex d-lg-none align-items-center justify-content-center">
-              <button className="navbar-brand brand-logo-mini mov" type="button" ><img src={logo} alt="logo" /> E-Bookalypse</button>
+          <nav className={`${dashstyle.navbar} p-0 fixed-top d-flex flex-row`}>
+            <div className={`${dashstyle.navbarBrandWrapper} d-flex d-lg-none align-items-center justify-content-center`}>
+              <button className={`${dashstyle.navbarBrand} ${dashstyle.brandLogoMini} movvv `} type="button" ><img src={logo} alt="logo" /> E-Bookalypse</button>
             </div>
 
-            <div className="navbar-menu-wrapper flex-grow d-flex align-items-stretch">
+            <div className={`${dashstyle.navbarMenuWrapper} flex-grow d-flex align-items-stretch`}>
 
-              <button className="navbar-toggler align-self-center" type="button" data-toggle="minimize">
-                <span className="mdi mdi-menu mov"></span>
+              <button className={`${dashstyle.navbarToggler} align-self-center`} type="button" data-toggle="minimize">
+                <span className="mdi mdi-menu movvvv"></span>
               </button>
 
-              <ul className="navbar-nav w-100">
-                <li className="nav-item w-100">
-                  <form className="nav-link mt-2 mt-md-0 d-none d-lg-flex search">
+              <ul className={`${dashstyle.navbarNav} w-100`}>
+                <li className={`${dashstyle.navItem} w-100`}>
+                  <form className={`${dashstyle.navLink} mt-2 mt-md-0 d-none d-lg-flex ${dashstyle.search}`}>
                     <input type="text" className="form-control text-muted fs-5" placeholder="Search on E-Bookalypse" />
                   </form>
                 </li>
               </ul>
-              <ul className="navbar-nav navbar-nav-right">
+              <ul className={`${dashstyle.navbarNav} ${dashstyle.navbarNavRight}`}>
 
-                <li className="nav-item dropdown border-left">
+                <li className={`${dashstyle.navItem} ${dashstyle.dropdown} border-left`}>
                 {/* eslint-disable-next-line */}
-                  <a className="nav-link count-indicator" href="" >
+                  <a className={`${dashstyle.navLink} ${dashstyle.countIndicator}`} href="" >
                     <i className="mdi mdi-bell" id='hovericon'></i>
-                    <span className="count bg-danger"></span>
+                    <span className={`${dashstyle.count} ${dashstyle.bgDanger}`}></span>
                   </a>
 
                 </li>
-                <li className="nav-item dropdown">
+                <li className={`${dashstyle.navItem} ${dashstyle.dropdown}`}>
                 {/* eslint-disable-next-line */}
-                  <a className="nav-link" href="">
-                    <div className="navbar-profile">
-                      <img className="img-xs rounded-circle" src={userImg} alt="" />
-                      <p className="mb-0 d-none d-sm-block navbar-profile-name" id='hovericon'>{user.fName + ' ' + user.lName}</p>
+                  <a className={`${dashstyle.navLink}`} href="">
+                    <div className={`${dashstyle.navbarProfile}`}>
+                      <img className={`${dashstyle.imgXs} rounded-circle`} src={userImg} alt="" />
+                      <p className={`mb-0 d-none d-sm-block ${dashstyle.navbarProfileName}`} id='hovericon'>{user.fName + ' ' + user.lName}</p>
 
                     </div>
                   </a>
 
                 </li>
               </ul>
-              <button className="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas" >
+              <button className={`${dashstyle.navbarToggler} ${dashstyle.navbarTogglerRight} d-lg-none align-self-center`} type="button" data-toggle="offcanvas" >
                 <span className="mdi mdi-format-line-spacing" id='hovericon' ></span>
               </button>
             </div>
--- src/components/AdminSidebar/AdminSidebar.js
+++ src/components/AdminSidebar/AdminSidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import '../../assetsAdmin/css/style.css'
+import dashstyle from '../../assetsAdmin/css/style.module.css'
 import "../../assetsAdmin/vendors/mdi/css/materialdesignicons.min.css"
 import "../../assetsAdmin/vendors/flag-icon-css/css/flag-icon.min.css"
 import "../../assetsAdmin/AdminDashboard.css";
@@ -12,21 +12,21 @@ function AdminSidebar(props) {
     if (user) {
         return (
             // <div>
-            <div className="sidebar sidebar-offcanvas" id="sidebar" >
+            <div className={`${dashstyle.sidebar} ${dashstyle.sidebarOffcanvas}`} id="sidebar" >
 
-                <div className="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top">
-                    <Link className="sidebar-brand brand-logo" to=""></Link>
+                <div className={`${dashstyle.sidebarBrandWrapper} d-none d-lg-flex align-items-center justify-content-center fixed-top`}>
+                    <Link className={`${dashstyle.sidebarBrand} ${dashstyle.brandLogo}`} to=""></Link>
                 </div>
 
-                <ul className="nav ">
-                    <li className="nav-item profile">
-                        <div className="profile-desc pt-5">
-                            <div className="profile-pic">
-                                <div className="count-indicator">
-                                    <img className="img-xs rounded-circle " src={userImg} alt="" />
-                                    <span className="count bg-success"></span>
+                <ul className={`${dashstyle.nav}`}>
+                    <li className={`${dashstyle.navItem} ${dashstyle.profile}`}>
+                        <div className={`${dashstyle.profileDesc} pt-5`}>
+                            <div className={`${dashstyle.profilePic}`}>
+                                <div className={`${dashstyle.countIndicator}`}>
+                                    <img className={`${dashstyle.imgXs} rounded-circle`}  src={userImg} alt="" />
+                                <span className={`${dashstyle.count} ${dashstyle.bgSuccess}`}></span>
                                 </div>
-                                <div className="profile-name">
+                                <div className={`${dashstyle.profileName}`}>
                                     <h5 className="mb-0 font-weight-bold text-white">{user.fName + " " + user.lName}</h5>
                                     <span >Admin</span>
                                 </div>
@@ -34,67 +34,67 @@ function AdminSidebar(props) {
 
                         </div>
                     </li>
-                    <li className="nav-item nav-category">
-                        <span className="nav-link">Navigation</span>
+                    <li className={`${dashstyle.navItem} ${dashstyle.navCategory}`}>
+                        <span className={`${dashstyle.navLink}`}>Navigation</span>
                     </li>
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-speedometer"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Dashboard</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Dashboard</span>
                         </Link>
                     </li>
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/books">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/books">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-book"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Books</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Books</span>
                         </Link>
                     </li>
 
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/categories">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/categories">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-table-large"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Categories</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Categories</span>
                         </Link>
                     </li>
 
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/users">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/users">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-contacts"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Users</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Users</span>
                         </Link>
                     </li>
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/collections">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/collections">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-playlist-play"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Collections</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Collections</span>
                         </Link>
                     </li>
 
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/promotions">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/promotions">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-history"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Promotions</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Promotions</span>
                         </Link>
                     </li>
 
-                    <li className="nav-item menu-items">
-                        <Link className="nav-link" to="/admin/writers">
-                            <span className="menu-icon">
+                    <li className={`${dashstyle.navItem} ${dashstyle.menuItems}`}>
+                        <Link className={`${dashstyle.navLink}`} to="/admin/writers">
+                            <span className={`${dashstyle.menuIcon}`}>
                                 <i className="mdi mdi-file-alert"></i>
                             </span>
-                            <span className="menu-title fs-5 text-muted" id="navigatehover">Writers</span>
+                            <span className={`${dashstyle.menuTitle} fs-5 text-muted`} id="navigatehover">Writers</span>
                         </Link>
                     </li>
                 </ul>
