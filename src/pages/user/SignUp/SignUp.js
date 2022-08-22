10a9d5bcb875f0a6924160a3498f3e7620b60459
Hussein Alaa <74919905+husseinalaa15@users.noreply.github.com>
2022-08-22 09:47:12 +0200

    First Release Test 1.0.4

--- src/pages/user/SignUp/SignUp.js
+++ src/pages/user/SignUp/SignUp.js
@@ -1,8 +1,12 @@
 import React, { lazy , Suspense } from 'react'
 import Preloader from '../../../components/Preloader/Preloader';
+import { useSelector } from 'react-redux';
+
 const SignUpForm = lazy(() => import('../../../components/SignUpForm/SignUpForm') );
 
 const SignUp = props => {
+  const theme = useSelector((state)=>state.theme.currentTheme)
+
   return (
     <div className={`content  ${theme === 'night' ? 'bg-dark' : 'bg-light' }`} style={{backgroundImage:"url(../../images/login.jpeg)",backgroundSize:"cover" , padding:"50px"}}>
       <Suspense fallback={<Preloader />}>
